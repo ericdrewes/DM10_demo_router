f2be1c01f6bb7c71be95010dfeb1064272fa73d7
ericdrewes <eric.d.drewes@gmail.com>
2017-12-12 11:51:16 -0600

    finished demo

--- src/App.js
+++ src/App.js
@@ -1,32 +1,32 @@
+
 import React, { Component } from "react";
 import { Navbar } from "react-bootstrap";
-import {HashRouter, Route, NavLink, Switch} from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import "./App.css";
 
 import Home from "./Home";
 import About from "./About";
 import Contact from "./Contact";
 
+import routes from './routes';
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <HashRouter>
+       
           <div>
-          <Navbar bsClass="App-header">
-            <div className="flex">
-              <NavLink activeClassName="active" exact to="/"> Home </NavLink>
-              <NavLink activeClassName="active" to="/About"> About </NavLink>
-              <NavLink activeClassName="active" to="/Contact"> Contact </NavLink>
-            </div>
-          </Navbar>
-            <Switch className="">
-              <Route exact path='/' component={Home} />
-              <Route path='/About' component={About} />
-              <Route path='/Contact' component={Contact} />
-            </Switch>
+            <Navbar bsClass="App-header">
+              <div className="flex">
+                <NavLink activeClassName="active" exact to="/"> Home </NavLink>
+                <NavLink activeClassName="active" to="/About"> About </NavLink>
+                <NavLink activeClassName="active" to="/Contact"> Contact </NavLink>
+                <NavLink activeClassName="active" to={`/Random/${Math.floor(Math.random() * 50)}`}> Random </NavLink>
+              </div>
+            </Navbar>
+             {routes}
           </div>
-        </HashRouter>
+     
       </div>
     );
   }
---
+++ src/routes.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {Switch, Route} from 'react-router-dom';
+import Home from './Home.js';
+import About from './About.js';
+import Contact from './Contact.js';
+
+
+export default (
+    <Switch className="">
+        <Route exact path='/' component={Home} />
+        <Route path='/About' component={About} />
+        <Route path='/Contact' component={Contact} />
+        <Route path='/Random/:num' 
+            render={(props) => {
+                console.log(props);
+                return <div>Random: {props.match.params.num}</div>
+        }} />
+  </Switch>
+);
